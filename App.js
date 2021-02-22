3d21c8dceb37c7e9929ec82f7bde3d854a31b9ea
David Iturriaga Sotelo <e-diturriaga@neoris.com>
2021-02-22 08:02:49 -0800

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, Component } from 'react';
+import { StyleSheet, Text, View, Switch, TextInput } from 'react-native';
 
 export default function App() {
+  // Switch Variables.
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  // Text Inputs Variables.
+  const [value, onChangeText] = React.useState('Useless Placeholder');
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Switch
+        trackColor={{ false: '#767577', true: '#81b0ff' }}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+      <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => onChangeText(text)}
+      value={value}
+    />
     </View>
   );
 }
